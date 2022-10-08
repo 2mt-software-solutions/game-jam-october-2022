0a594fad983dd0d29317d334f7708185fc62cde2
Sebastian Heuser <sebastian.heuser@2mt.info>
2022-10-08 16:16:01 +0200

    remember last date of app-exit / persistent state-storage

--- Game.cs
+++ Game.cs
@@ -1,13 +1,25 @@
-﻿using Microsoft.Xna.Framework;
+﻿using Android.Content;
+using Android.Media;
+using Android.OS;
+using Android.Util;
+using Microsoft.VisualBasic;
+using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using System;
+using System.IO.IsolatedStorage;
+using tamagotchi.Gamestates;
 
 namespace tamagotchi
 {
     public class Game : Microsoft.Xna.Framework.Game
     {
+        private const string SAVEGAME = "savegame";
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
+        private Gamestate _state;
+        private long _lastExit;
+        private IsolatedStorageFile savegameStorage = IsolatedStorageFile.GetUserStoreForApplication();
 
         public Game()
         {
@@ -18,8 +30,9 @@ namespace tamagotchi
 
         protected override void Initialize()
         {
-            // TODO: Add your initialization logic here
+            this._state = new RunningState();
 
+            this.readSaveGame();
             base.Initialize();
         }
 
@@ -27,15 +40,20 @@ namespace tamagotchi
         {
             _spriteBatch = new SpriteBatch(GraphicsDevice);
 
-            // TODO: use this.Content to load your game content here
         }
 
         protected override void Update(GameTime gameTime)
         {
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+            {
+                this._lastExit = DateAndTime.Now.Ticks;
+                this.writeSavegame();
                 Exit();
+            }
 
-            // TODO: Add your update logic here
+            this._state.handleInput(GamePad.GetState(PlayerIndex.One), Keyboard.GetState());
+            this._state.update();
+
 
             base.Update(gameTime);
         }
@@ -44,9 +62,47 @@ namespace tamagotchi
         {
             GraphicsDevice.Clear(Color.CornflowerBlue);
 
-            // TODO: Add your drawing code here
-
             base.Draw(gameTime);
+        }
+
+        protected void readSaveGame()
+        {
+
+            if (savegameStorage.FileExists(SAVEGAME))
+            {
+                IsolatedStorageFileStream fs = null;
+                try
+                {
+                    fs = savegameStorage.OpenFile(SAVEGAME, System.IO.FileMode.Open);
+                }
+                catch (IsolatedStorageException e)
+                {
+                    this._lastExit = DateAndTime.Now.Ticks;
+                }
+
+                if (fs != null)
+                {
+                    byte[] saveBytes = new byte[256];
+                    int count = fs.Read(saveBytes, 0, 256);
+                    string lastExit = System.Text.Encoding.UTF8.GetString(saveBytes, 0, count);
+                    fs.Close();
+
+                    this._lastExit = Convert.ToInt64(lastExit);
+                    Log.Debug(null, "last exit: " + this._lastExit.ToString());
+
+                }
+            }
+        }
+
+        protected void writeSavegame()
+        {
+            IsolatedStorageFileStream fs = null;
+            fs = savegameStorage.OpenFile(SAVEGAME, System.IO.FileMode.Create);
+            if (fs != null)
+            {
+                fs.Write(System.Text.Encoding.UTF8.GetBytes(this._lastExit.ToString()));
+                fs.Close();
+            }
         }
     }
 }
---
+++ Gamestate.cs
@@ -0,0 +1,25 @@
+﻿using Android.Icu.Text;
+using Java.Util.Functions;
+using Microsoft.Xna.Framework.Input;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tamagotchi
+{
+    abstract class Gamestate
+    {
+        public void update()
+        {
+            return;
+        }
+
+        public void handleInput(GamePadState gamepad, KeyboardState keyboard)
+        {
+            return;
+        }
+    }
+}
