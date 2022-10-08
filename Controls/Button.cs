72f4c4cdf7f357ef7c383cf8661695fc049088e9
2mt-doerr <fabian.doerr@2mt.info>
2022-10-08 16:24:59 +0200

    add quit button

---
+++ Controls/Button.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Input.Touch;
+
+namespace tamagotchi.Controls
+{
+    public class Button : Component
+    {
+        #region Fields
+
+        private MouseState _currentMouse;
+
+        private SpriteFont _font;
+
+        private bool _isHovering;
+
+        private MouseState _previousMouse;
+
+        private Texture2D _texture;
+
+        #endregion
+
+        #region Properties
+
+        public event EventHandler Touch;
+
+        public bool Clicked { get; private set; }
+
+        public Color PenColour { get; set; }
+
+        public Vector2 Position { get; set; }
+
+        public Rectangle Rectangle
+        {
+            get
+            {
+                return new Rectangle((int)Position.X, (int)Position.Y, _texture.Width, _texture.Height);
+            }
+        }
+
+        public string Text { get; set; }
+
+        #endregion
+
+        #region Methods
+
+        public Button(Texture2D texture, SpriteFont font)
+        {
+            _texture = texture;
+
+            _font = font;
+
+            PenColour = Color.Black;
+        }
+
+        public override void Draw(GameTime gameTime, SpriteBatch spriteBatch)
+        {
+            var colour = Color.White;
+
+            if (_isHovering)
+                colour = Color.Gray;
+
+            spriteBatch.Draw(_texture, Rectangle, colour);
+
+            if (!string.IsNullOrEmpty(Text))
+            {
+                var x = (Rectangle.X + (Rectangle.Width / 2)) - (_font.MeasureString(Text).X / 2);
+                var y = (Rectangle.Y + (Rectangle.Height / 2)) - (_font.MeasureString(Text).Y / 2);
+
+                spriteBatch.DrawString(_font, Text, new Vector2(x, y), PenColour);
+            }
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            TouchCollection touchCollection = TouchPanel.GetState();
+            if(CheckTouch(Rectangle, touchCollection))
+            {
+                Touch?.Invoke(this, new EventArgs());
+            }
+        }
+
+        private bool CheckTouch(Rectangle target, TouchCollection touchCollection)
+        {
+            if (touchCollection.Count > 0)
+            {
+                foreach (var touch in touchCollection)
+                {
+                    if (target.Contains(touch.Position))
+                    {
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+
+        #endregion
+    }
+}
--- Game1.cs
+++ Game1.cs
@@ -1,13 +1,21 @@
-﻿using Microsoft.Xna.Framework;
+﻿using Android.Views;
+using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Input.Touch;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using tamagotchi.Controls;
 
 namespace tamagotchi
 {
     public class Game1 : Game
-    {
+    { 
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
+
+        private List<Component> _gameComponents;
 
         public Game1()
         {
@@ -28,6 +36,23 @@ namespace tamagotchi
             _spriteBatch = new SpriteBatch(GraphicsDevice);
 
             // TODO: use this.Content to load your game content here
+            var quitButton = new Button(Content.Load<Texture2D>("Controls/Button"), Content.Load<SpriteFont>("Fonts/Font"))
+            {
+                Position = new Vector2(100, 250),
+                Text = "Quit",
+            };
+            quitButton.Touch += QuitButton_Touch;
+            _gameComponents = new List<Component>()
+            {
+                quitButton,
+            };
+
+        }
+
+        private void QuitButton_Touch(object sender, System.EventArgs e)
+        {
+            Console.WriteLine("test");
+            Exit();
         }
 
         protected override void Update(GameTime gameTime)
@@ -36,6 +61,8 @@ namespace tamagotchi
                 Exit();
 
             // TODO: Add your update logic here
+            foreach (var component in _gameComponents)
+                component.Update(gameTime);
 
             base.Update(gameTime);
         }
@@ -45,6 +72,12 @@ namespace tamagotchi
             GraphicsDevice.Clear(Color.CornflowerBlue);
 
             // TODO: Add your drawing code here
+            _spriteBatch.Begin();
+
+            foreach (var component in _gameComponents)
+                component.Draw(gameTime, _spriteBatch);
+
+            _spriteBatch.End();
 
             base.Draw(gameTime);
         }
