bc8678bd68088a65629723d72472abe8dde8f6de
Jan Schwarz <jan.schwarz@2mt.info>
2022-10-08 14:50:04 +0200

    add character class

---
+++ Character.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tamagotchi
+{
+    internal class Character
+    {
+        // attributes
+        private int playerHealth;
+        private Texture2D playerTexture;
+        protected int playerWidth;
+        protected int playerHeight;
+        protected Game game;
+
+        public Character(Game game)
+        {
+            this.game = game;
+            playerWidth = game.GraphicsDevice.Viewport.Width;
+            playerHeight = game.GraphicsDevice.Viewport.Height;
+            this.playerTexture = game.Content.Load<Texture2D>("player-dino");
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Begin();
+            playerTexture.Draw(spriteBatch);
+            spriteBatch.End();
+        }
+    }
+}
